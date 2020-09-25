8dd7be119fd5cb6507c1fd0fa08dc0b0dd102abb
XuanThai <mknumberone9x@gmail.com>
2020-09-25 22:47:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//=============Bai 11: Đếm có bao nhiêu khoảng trắng trong một chuỗi?
+// var str= " Hello  World"
+// var spaceCount = str.split(" ").length - 1
+// console.log(spaceCount);
+//=============Bai 12:Hãy remove tất cả khoảng trắng, space và ‘enter’ trong một chuỗi
+// var str= " Hello  World"
+// var removeSpace = str.replace(/\s+/g,'')
+// console.log(removeSpace);
+//============Bai 13:Hãy viết chương trình đảo ngược sau.
+// var str = "This is a beautiful day"
+// var reveserStr= str.split("").reverse().join("")
+// console.log(reveserStr);
+//============Bai 14:Nhập vào chuỗi s1 và s2, cho biết vị trí xuất hiện của chuỗi s2 trong s1.
+var str = "This is beautiful day"
+var str1 = "is"
+
+
