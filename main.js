17aee53a659448befc33cdbf4f5d501a0c8a9aee
XuanThai <mknumberone9x@gmail.com>
2020-09-29 15:53:10 +0700

    them bai moi

--- main.js
+++ main.js
@@ -11,7 +11,49 @@
 // var reveserStr= str.split("").reverse().join("")
 // console.log(reveserStr);
 //============Bai 14:Nhập vào chuỗi s1 và s2, cho biết vị trí xuất hiện của chuỗi s2 trong s1.
-var str = "This is beautiful day"
-var str1 = "is"
-
+// var str2="is"
+// var str = `This ${str2} beautiful day`
+// var pos =str.indexOf(`${str2}`)
+// console.log(pos);
+//===========Bai 15: Viết chương trình lấy  id name của fb.
+// var srcFb="https://www.facebook.com/ngothucdat"
+// var getName=srcFb.slice(25)
+// console.log(getName);
+//===========Bai 16: Viết chương trình chuẩn hóa họ tên.
+// var str="NguyeN THI tho tHu hA"
+// var trueName=str.toLowerCase().split(' ').map(x=>x[0].toUpperCase()+x.slice(1)).join(' ');
+// console.log(trueName);
+//===========Bai 17:Viết hàm số tính min, max của 3 số
+//a tim max
+// function findMax(a,b,c){
+//      if(a>b && a>c){
+//          return a
+//      }if(b>c && b>a){
+//          return b
+//      }if(c>a && c>b){
+//          return c
+//      }
+//     }
+// console.log(findMax(2,4,7));
+//b tim min
+// function findMin(a,b,c){
+//     if(a<b && a<c){
+//         return a
+//     }if(b<c && b<a){
+//         return b
+//     }if(c<a && c<b){
+//      return c
+//     }
+// }
+// console.log(findMin(2,4,7));
+//===========Bai 18:Viết hàm tìm vị trí xâu X tồn tại kề cuối trong xâu Y
+// var str =`This is a beautiful day, is it?`
+// var arr = str.split(" ")
+// for(var i = arr.length;i>=0;i--){
+//     if(arr[i]=="is"){
+//         console.log(arr[i]+"-"+i);
+//         break
+//     }
+// }
+//===========Bai 19:Hãy tính tổng các số từ 1 đến n mà không dùng for, while loop?
 
