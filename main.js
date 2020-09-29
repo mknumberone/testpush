b4309a72efd5a6d76a70455d73c7a359768a4097
XuanThai <mknumberone9x@gmail.com>
2020-09-29 16:29:21 +0700

    Them bai moi

--- main.js
+++ main.js
@@ -56,4 +56,8 @@
 //     }
 // }
 //===========Bai 19:Hãy tính tổng các số từ 1 đến n mà không dùng for, while loop?
-
+// function sum(n) {
+//     if (n < 1) return 0;
+//     return n  + sum(n - 1); 
+// }
+// console.log(sum(9));
